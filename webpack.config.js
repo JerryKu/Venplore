c7bb35852ad8cc051fea50f1435dffa26160b215
Andrew Chen <andrewchen.c44@gmail.com>
2018-01-30 01:40:13 -0800

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,24 @@
-const path              = require('path');
-const webpack           = require('webpack');
-const htmlPlugin        = require('html-webpack-plugin');
-const openBrowserPlugin = require('open-browser-webpack-plugin'); 
-const dashboardPlugin   = require('webpack-dashboard/plugin');
-const autoprefixer      = require('autoprefixer'); 
 
-const PATHS = {
-  app: path.join(__dirname, 'app/client/src'),
-  images:path.join(__dirname,'app/client/src/assets/'),
-  build: path.join(__dirname, 'app/client/dist')
-};
-
-const options = {
-  host:'localhost',
-  port:'8000'
-};
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: {
-    app: `${PATHS.app}/index.jsx`
-  }
-,
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
-    path: PATHS.build,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: DIST_DIR
   },
-  devServer: {
-      historyApiFallback: true,
-      hot: true,
-      inline: true,
-      stats: 'errors-only',
-      host: options.host,
-      port: options.port 
-    },
-  module: {
-    loaders: [
+  module : {
+    loaders : [
       {
-        test: /\.js$/,
-        include: PATHS.build,
-        loader: 'babel-loader',
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',      
         query: {
-          presets: ['es2015', 'react', 'stage-2']
-        }
-      },
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css', 'postcss'],
-        include:PATHS.app
-      },
-      
-      {
-        test: /\.(ico|jpg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,        
-        loader: 'file',
-        query: {
-          name: '[path][name].[ext]'
-        }
-      },      
+          presets: ['react', 'es2015']
+       }
+      }
     ]
-  },
-  postcss: function() {
-    return [
-      autoprefixer({
-        browsers: [
-          '>1%',
-          'last 4 versions',
-          'Firefox ESR',
-          'not ie < 9',
-        ]
-      }),
-    ];
-  },
-  plugins:[
-    new dashboardPlugin(),
-    new webpack.HotModuleReplacementPlugin({
-        multiStep: true
-    }),
-    new htmlPlugin({
-      template:path.join(PATHS.app,'index.html'),
-      inject:'body'
-    }),
-    new openBrowserPlugin({
-      url: `http://${options.host}:${options.port}`
-    })
-  ]
+  }
 };

3c10c6774f8b18832d1f405a6583cc7be24385bf
Andrew Chen <andrewchen.c44@gmail.com>
2018-01-30 02:06:31 -0800

    Add server script

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 
 app.use(express.static(__dirname + '/../client'));
 app.use(express.static(__dirname + '/../node_modules'));
+
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
   console.log('listening on port 3000!');

7d25e4ba3c3490b7b8fcf6174bcf192f2d0f2cd7
Andrew Chen <andrewchen.c44@gmail.com>
2018-01-30 01:46:10 -0800

    Clean up server code

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var db = require('../database');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-var app = express();
+app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/../node_modules'));
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log('listening on port 3000!');
 });

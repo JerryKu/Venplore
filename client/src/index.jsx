ded1ccde8150534bfd841839e7a2a5a6a232cbd9
Andrew Chen <andrewchen.c44@gmail.com>
2018-02-04 21:23:18 -0800

    Add reduc to index.js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import {createStore, applyMiddleware, compose} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './Reducer';
 import App from './app.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+const RenderApp = () => {
+    render(
+      <Router>
+        <Provider store={store}>
+            <App store={store}/>
+        </Provider>
+      </Router>, document.getElementById('app'));
+  }
+  
+  RenderApp();

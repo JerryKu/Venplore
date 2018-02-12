b1216db9879171c41d079b3b7cd1b2b677ae298c
Andrew Chen <andrewchen.c44@gmail.com>
2018-02-11 21:23:49 -0800

    edit rendering for index.jsx

--- client/src/components/currentActivity.jsx
+++ client/src/components/currentActivity.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import currentActivity from './components/currentActivity.jsx'
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {}
   }
-  componentDidMount() {
-
-  }
 
   render() {
     return (
       <div>
-        <h1>Venplore</h1>
-        <currentActivity/>
+        <h1>FISHANGGGGG</h1>
       </div>
-      
     )
   }
 }
 
-export default app;
+export default currentActivity;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,24 +1,15 @@
 
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import reducer from './Reducer';
-import App from './app.jsx';
+// import reducer from './Reducer';
+import app from './app.jsx';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
-const RenderApp = () => {
-    render(
-      <Router>
-        <Provider store={store}>
-            <App store={store}/>
-        </Provider>
-      </Router>, document.getElementById('app'));
-  }
-  
-  RenderApp();
+ReactDOM.render(<app/>, document.getElementById('app'))

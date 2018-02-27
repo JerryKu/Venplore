209535af914f95d3a02b4e56afaaedca9b67bd55
Andrew Chen <andrewchen.c44@gmail.com>
2018-02-26 17:23:56 -0800

    Render placeholder image and description

--- client/src/components/currentActivity.jsx
+++ client/src/components/currentActivity.jsx
@@ -7,11 +7,13 @@ class CurrentActivity extends React.Component {
   }
 
   render() {
-    return (
-      <span>
-        Current Activity
-      </span>
-    );
+    return <span>
+        <div className="main-image-section">
+          <img className="main-image" src="http://ichef.bbci.co.uk/wwfeatures/wm/live/1280_640/images/live/p0/51/v6/p051v6vn.jpg" alt="adventure" />
+        </div>
+
+        <div className="description-section">Adventure Awaits!</div>
+      </span>;
   }
 }
 

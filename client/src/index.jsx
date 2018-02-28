a398582ae41f888713b1e417bd9bc6054235b7a8
Andrew Chen <andrewchen.c44@gmail.com>
2018-02-27 17:03:48 -0800

    Add store to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,4 +7,10 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import App from './App.jsx'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+
+
 ReactDOM.render(<App />, document.getElementById('app'));

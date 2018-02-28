f875df8743750b1887e5bda9fe60209c3d9cc9be
Andrew Chen <andrewchen.c44@gmail.com>
2018-02-28 13:22:31 -0800

    Remove redux in current activity

--- client/src/components/currentActivity.jsx
+++ client/src/components/currentActivity.jsx
@@ -9,13 +9,13 @@ class CurrentActivity extends React.Component {
     };
   }
   
-  updateActivity() {
-    this.setState({
-      image: store.getState().image,
-      description: store.getState().description
-    })
-    console.log(this.state)
-  }
+  // updateActivity() {
+  //   this.setState({
+  //     image: store.getState().image,
+  //     description: store.getState().description
+  //   })
+  //   console.log(this.state)
+  // }
 
 
   render() {
@@ -23,7 +23,7 @@ class CurrentActivity extends React.Component {
         <div className="main-image-section">
           {/* <img className="main-image" src={"http://ichef.bbci.co.uk/wwfeatures/wm/live/1280_640/images/live/p0/51/v6/p051v6vn.jpg"} alt="adventure" /> */}
           {this.state.image}
-          {this.updateActivity()}
+          {/* {this.updateActivity()} */}
         </div>
 
         <div className="description-section">Adventure Awaits!</div>

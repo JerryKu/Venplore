e721879b4b5b21d0302f99e4742b97b058fbcfd9
Andrew Chen <andrewchen.c44@gmail.com>
2018-03-22 18:40:02 -0700

    Successfully connect to mongodb atlas

--- database/index.js
+++ database/index.js
@@ -1,1 +1,13 @@
-const mongo = require('mongodb');
+const MongoClient = require("mongodb").MongoClient;
+const config = require('../config.js');
+
+const uri =
+`mongodb://master:${config.dbPass}@venplore-shard-00-00-tw68l.mongodb.net:27017,venplore-shard-00-01-tw68l.mongodb.net:27017,venplore-shard-00-02-tw68l.mongodb.net:27017/test?ssl=true&replicaSet=Venplore-shard-0&authSource=admin`;
+
+
+const db = MongoClient.connect(uri, (err) => {
+  if(err) throw err;
+  console.log('connection opened');
+});
+
+module.exports = db;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const db = require('../database/index.js')
 
 const app = express();
 const port = process.env.PORT || 8080;

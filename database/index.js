7def81ac3778caaa5b389a7b7ed175a7768f1932
Andrew Chen <andrewchen.c44@gmail.com>
2018-03-25 22:22:52 -0700

    Clean up databse index file

--- database/index.js
+++ database/index.js
@@ -1,13 +1,15 @@
-const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const config = require('../config.js');
 
 const uri =
 `mongodb://master:${config.dbPass}@venplore-shard-00-00-tw68l.mongodb.net:27017,venplore-shard-00-01-tw68l.mongodb.net:27017,venplore-shard-00-02-tw68l.mongodb.net:27017/test?ssl=true&replicaSet=Venplore-shard-0&authSource=admin`;
 
-
 const db = MongoClient.connect(uri, (err) => {
-  if(err) throw err;
-  console.log('connection opened');
+  if (err) {
+    throw err;
+  } else {
+    console.log('Database Connection Open');
+  }
 });
 
 module.exports = db;

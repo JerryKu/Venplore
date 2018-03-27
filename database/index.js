659609f37cd5681f2dc0e4a40bdf425751c963ac
Andrew Chen <andrewchen.c44@gmail.com>
2018-03-26 17:18:34 -0700

    Successfully inserted data

--- database/index.js
+++ database/index.js
@@ -3,13 +3,22 @@ const config = require('../config.js');
 
 const uri =
 `mongodb://master:${config.dbPass}@venplore-shard-00-00-tw68l.mongodb.net:27017,venplore-shard-00-01-tw68l.mongodb.net:27017,venplore-shard-00-02-tw68l.mongodb.net:27017/test?ssl=true&replicaSet=Venplore-shard-0&authSource=admin`;
+let db = null;
 
-const db = MongoClient.connect(uri, (err) => {
+MongoClient.connect(uri, (err, client) => {
   if (err) {
     throw err;
   } else {
+    db = client.db('activities');
     console.log('Database Connection Open');
+    db.collection('activities').insertOne({
+  name: 'fishing',
+}).then((results) => {
+  console.log(results);
+});
   }
 });
+
+
 
 module.exports = db;

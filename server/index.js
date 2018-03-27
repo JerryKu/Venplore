22ee957e9a807562d82c40be1e807940282edec4
Andrew Chen <andrewchen.c44@gmail.com>
2018-03-27 16:10:24 -0700

    Implement skeleton add event route

--- server/index.js
+++ server/index.js
@@ -2,14 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const db = require('../database/index.js')
+const addEvent = require()
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
-
 app.use(bodyParser.json());
 
-app.listen(port, () => {
+
+app.get('/event', (request, response) => {
+
+
+});
+
+const server = app.listen(port, (err) => {
+  if(err) throw err;
   console.log('listening on port', port);
 });
+
+module.exports = server;

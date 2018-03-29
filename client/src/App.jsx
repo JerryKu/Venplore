3734617ffa45f7ed9f01455d0cf6397c9b37b583
Andrew Chen <andrewchen.c44@gmail.com>
2018-03-28 21:05:27 -0700

    Move individual components to wrapper components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-// import CurrentActivity from './components/CurrentActivity.jsx';
-// import FilterBar from './components/FilterBar.jsx';
-// import SearchBar from './components/SearchBar.jsx';
-// import ActivitiesList from './components/ActivitiesList.jsx';
+
 import HeaderBar from './components/HeaderBar.jsx';
 import EventCreator from './components/EventCreator.jsx';
 import ActivityFinder from './components/ActivityFinder.jsx';
@@ -21,21 +18,6 @@ class App extends React.Component {
           <Route exact path='/' component={ActivityFinder}/>  
           <Route path='/create' component={EventCreator}/>
         </Switch>
-
-          {/* <div className='filter-section'>
-            <div>
-              <SearchBar />
-            </div>
-            <div>
-              <FilterBar />
-            </div>
-          </div>
-          <div className='activity-list-section'>
-          <ActivitiesList />
-          </div>
-          <div className='current-activity-section'>
-            <CurrentActivity />
-          </div> */}
         </div>;
     }
   }
--- client/src/components/ActivityFinder.jsx
+++ client/src/components/ActivityFinder.jsx
@@ -1,18 +1,30 @@
 import React from "react";
+import CurrentActivity from './CurrentActivity.jsx';
+import FilterBar from './FilterBar.jsx';
+import SearchBar from './SearchBar.jsx';
+import ActivitiesList from './ActivitiesList.jsx';
 import { Link } from 'react-router-dom';
 
 class ActivityFinder extends React.Component {
   constructor(){
     super();
-
   }
 
   render(){
-    return <span>
-      activity finder page
-
-    </span>;
-
+    return <div className='filter-section'>
+      <div>
+        <SearchBar />
+      </div>
+      <div>
+        <FilterBar />
+      </div>
+      <div className='activity-list-section'>
+      <ActivitiesList />
+      </div>
+      <div className='current-activity-section'>
+        <CurrentActivity />
+      </div>
+    </div>
   }
 }
 

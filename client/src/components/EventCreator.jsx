20a82c6b34dc4b07fe2b37b7f9d99854b2b632e2
Andrew Chen <andrewchen.c44@gmail.com>
2018-03-28 18:14:33 -0700

    Add navigation to event creator page

--- client/src/components/EventCreator.jsx
+++ client/src/components/EventCreator.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
 
 class EventCreator extends React.Component {
   constructor(){
     super();
 
   }
-
   render(){
     return <span>
       event creation page
+    <Link to='/'>To Finder</Link>
     </span>;
-
   }
 }
 
-export default EventCrator;
+export default EventCreator;

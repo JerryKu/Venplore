a482a5cf79adc21ee731ba0a05bb2fd1829afb3f
Andrew Chen <andrewchen.c44@gmail.com>
2018-03-30 13:48:53 -0700

    Create skeleton html for event creator and set as default route

--- client/src/components/eventCreating/EventCreator.jsx
+++ client/src/components/eventCreating/EventCreator.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React from 'react';
 
 class EventCreator extends React.Component {
   constructor(){
     super();
   }
   render(){
-    return <span>
-      event creation page
-    </span>;
+    return <div className='event-creation'>
+      <form>
+        <label> Activity Name:<input type='text' name='Activity Name' /> </label> <br />
+        <label> Image: <input type='text' name='Image' /> </label> <br />
+        <label> Description: <input type='text' name='Description' /> </label> <br />
+        <label> Cost: <input type='text' name='Cost' /> </label> <br />
+        <label> Physical Effort: <input type='text' name='Physical Effort' /> </label> <br />
+        <label> Mental Effort: <input type='text' name='Mental Effort' /> </label> <br />
+        <label> Duration: <input type='text' name='Duration' /> </label> <br />
+        <input type='submit' value='Submit' />
+      </form>
+    </div>;
   }
 }
 

d4e78b71079eab028875b1d8aee86e3c9c630b66
Andrew Chen <andrewchen.c44@gmail.com>
2018-04-12 15:22:23 -0700

    Render current rating levels

--- client/src/components/eventCreating/EventCreator.jsx
+++ client/src/components/eventCreating/EventCreator.jsx
@@ -6,62 +6,59 @@ class EventCreator extends React.Component {
     this.state = {
         activityName: '',
         imageLink: '',
-        overallEnjoyability: 0,
-        cost: 0,
-        mentalEffort: 0,
-        physicalEffort: 0,
-        natureLevel: 0,
-        socialLevel: 0,
-        duration: 0,
+        overallEnjoyability: 5,
+        cost: 1,
+        mentalEffort: 1,
+        physicalEffort: 1,
+        natureLevel: 5,
+        socialLevel: 3,
+        duration: 5,
         description: '',
     }
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event){
-     console.log(this.state);
-     let newState = {};
-     newState[event.target.name] = event.target.value;
+    let newState = {};
+    newState[event.target.name] = event.target.value;
     this.setState(newState);
-   
   }
 
   render(){
     return <div style={{ height: "100%" }}>
         <form className="event-creation">
           <label>
-            Activity Name: <input type="text" name="activityName" placeholder="Fishing" onChange={this.handleChange}/>
+            Activity Name: <input type="text" name="activityName" placeholder="Fishing" value={this.state.activityName} onChange={this.handleChange}/>
           </label> <br />
           <label>
-            Image Link: <input type="text" name="imageLink" placeholder="https://res.cloudinary.com/simpleview/image/upload/c_fill,f_auto,q_65,w_768/v1/clients/norway/Fishing_2152634f-ba23-4044-8145-6ec3bab642bf.png"  onChange={this.handleChange}/>
+            Image Link: <input type="text" name="imageLink" value={this.state.imageLink}placeholder="https://res.cloudinary.com/simpleview/image/upload/c_fill,f_auto,q_65,w_768/v1/clients/norway/Fishing_2152634f-ba23-4044-8145-6ec3bab642bf.png"  onChange={this.handleChange}/>
           </label> <br />
           <label>
-            Overall Enjoyability: <input type="range" min="0" max="5" step="1" name='overallEnjoyability' onChange={this.handleChange}/>
+            Overall Enjoyability: <input type="range" min="0" max="5" step="1" name='overallEnjoyability' value={this.state.overallEnjoyability} onChange={this.handleChange}/>
           </label> <br />
           <label>
-            Cost: <input type="range" min="0" max="5" step="1" name='cost' onChange={this.handleChange}/>
+            Cost: {this.state.cost} <input type="range" min="0" max="5" step="1" name='cost' value={this.state.cost} onChange={this.handleChange}/>
           </label> <br />
           <label>
-            Mental Effort: <input type="range" min="0" max="5" step="1" name='mentalEffort' onChange={this.handleChange}/>
+            Mental Effort: {this.state.mentalEffort}<input type="range" min="0" max="5" step="1" name='mentalEffort' value={this.state.mentalEffort} onChange={this.handleChange}/>
           </label> <br />
           <label>
-            Physical Effort: <input type="range" min="0" max="5" step="1" name='physicalEffort' onChange={this.handleChange}/>
+            Physical Effort: {this.state.physicalEffort} <input type="range" min="0" max="5" step="1" name='physicalEffort' value={this.state.physicalEffort} onChange={this.handleChange}/>
           </label> <br />
           <label>
-            Nature Level: <input type="range" min="0" max="5" step="1" name='natureLevel' onChange={this.handleChange}/>
+            Nature Level: {this.state.natureLevel} <input type="range" min="0" max="5" step="1" name='natureLevel' value={this.state.natureLevel} onChange={this.handleChange}/>
           </label> <br />
           <label>
-            Social Level: <input type="range" min="0" max="5" step="1" name='socialLevel' onChange={this.handleChange}/>
+            Social Level: {this.state.socialLevel}<input type="range" min="0" max="5" step="1" name='socialLevel' value={this.state.socialLevel} onChange={this.handleChange}/>
           </label> <br />
           <label>
-            Duration: <input type="range" min="0" max="5" step="1" name='duration' onChange={this.handleChange}/>
+            Duration: {this.state.duration}<input type="range" min="0" max="5" step="1" name='duration' value={this.state.duration} onChange={this.handleChange}/>
           </label> <br />
           <label style={{ height: "20%" }}>
             Description: 
             <div>
-              <textarea rows='4' cols='100' className='description-box' name="description" placeholder='All you need is a chair and fishing rod to enjoy hours of relaxation by your favorite body of water.'  onChange={this.handleChange}/>
+              <textarea rows='4' cols='100' className='description-box' name="description" value={this.state.description} placeholder='All you need is a chair and fishing rod to enjoy hours of relaxation by your favorite body of water.'  onChange={this.handleChange}/>
             </div>
-
           </label> <br />
           <input className="create-button" type="submit" value="Create Adventure" />
         </form>

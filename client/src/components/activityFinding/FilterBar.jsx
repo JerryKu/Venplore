c22e1d52ec60d6271b8cde0a00bcdff5464f2725
Andrew Chen <andrewchen.c44@gmail.com>
2018-04-11 19:40:36 -0700

    Remove filter slider component

--- client/src/components/activityFinding/FilterBar.jsx
+++ client/src/components/activityFinding/FilterBar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import actions from '../../actions/index.jsx'
-// import FilterSlider from "./FilterSlider.jsx";
 
 class FilterBar extends React.Component {
   constructor(props) {

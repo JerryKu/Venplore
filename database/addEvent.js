9a6b1f661ed840974343b88a14923f7cd28a3a1f
Andrew Chen <andrewchen.c44@gmail.com>
2018-04-15 13:13:44 -0700

    Refactor headerbar to functional stateless component

--- database/addEvent.js
+++ database/addEvent.js
@@ -2,16 +2,20 @@ const connection = require('./index.js');
 
 const addEvent = (eventData) => {
   connection.db.collection('activities').insertOne({
-    name: eventData.activityName,
-    imageLink: eventData.imageLink,
-    overallEnjoyability: Number(eventData.overallEnjoyability),
-    cost: Number(eventData.cost),
-    mentalEffort: Number(eventData.mentalEffort),
-    physicalEffort: Number(eventData.physicalEffort),
-    natureLevel: Number(eventData.natureLevel),
-    socialLevel: Number(eventData.socialLevel),
-    duration: Number(eventData.duration),
-    description: eventData.description,
+    eventInfo: {
+      name: eventData.activityName,
+      imageLink: eventData.imageLink,
+      description: eventData.description,
+    },
+    eventRatings: {
+      overallEnjoyability: Number(eventData.overallEnjoyability),
+      cost: Number(eventData.cost),
+      mentalEffort: Number(eventData.mentalEffort),
+      physicalEffort: Number(eventData.physicalEffort),
+      natureLevel: Number(eventData.natureLevel),
+      socialLevel: Number(eventData.socialLevel),
+      duration: Number(eventData.duration),
+    },
   }).then((results) => {
     return results;
   });

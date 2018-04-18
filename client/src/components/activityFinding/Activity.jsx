65a17add0f9d57d71c2f9a038aaf95f2e3626a37
Andrew Chen <andrewchen.c44@gmail.com>
2018-04-18 11:28:57 -0700

    Add start script for heroku

--- client/src/components/activityFinding/Activity.jsx
+++ client/src/components/activityFinding/Activity.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Activity = ({image, name, score}) => {
   return <div className="activity-list-item">
-      <div className='activity-list-item-title'>{name}</div>
-      <div>Adventure Score: {score}</div>
+      <h1 />
+      <p>{name}</p>
+      <span>Adventure Score: {score}</span>
       <img className='activity-list-image' src={image} />
 
     </div>

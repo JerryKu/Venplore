803ee725ddfaffa6759eb1a3ec70ce7f1fac5577
Andrew Chen <andrewchen.c44@gmail.com>
2018-04-18 12:02:26 -0700

    set db pass to environment pass

--- database/index.js
+++ database/index.js
@@ -1,8 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
 const config = require('../config.js');
+const pass = process.env.dbPass || config.dbPass;
 
 const uri =
-`mongodb://master:${config.dbPass}@venplore-shard-00-00-tw68l.mongodb.net:27017,venplore-shard-00-01-tw68l.mongodb.net:27017,venplore-shard-00-02-tw68l.mongodb.net:27017/test?ssl=true&replicaSet=Venplore-shard-0&authSource=admin`;
+`mongodb://master:${pass}@venplore-shard-00-00-tw68l.mongodb.net:27017,venplore-shard-00-01-tw68l.mongodb.net:27017,venplore-shard-00-02-tw68l.mongodb.net:27017/test?ssl=true&replicaSet=Venplore-shard-0&authSource=admin`;
 let state = {
 
 };

d58f7024b7b1c962380169df159baf348e5b411a
Andrew Chen <andrewchen.c44@gmail.com>
2018-04-18 12:35:22 -0700

    Remove thunk for now

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 import venplore from './reducers/index.jsx';
 import App from './App.jsx'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(venplore, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(venplore);
 
 ReactDOM.render(
   <Router>

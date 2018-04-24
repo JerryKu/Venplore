6a4965a767ad293e3a68637e97549b228b472ade
Andrew Chen <andrewchen.c44@gmail.com>
2018-04-24 16:07:43 -0700

    set backgroun color for currently active tab

--- client/src/components/navigating/NavBar.jsx
+++ client/src/components/navigating/NavBar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 
 const HeaderBar = (props) => {
   return (
     <div className='header-bar'>
-      <Link to='/' className='header-tab'>Venplore - Venture, and Explore!</Link>
-      <Link to='/' className='header-tab'>Home</Link>
-      <Link to='/create' className='header-tab'>Create an activity </Link>
-      <Link to='/about' className='header-tab'>About</Link> 
+      <NavLink to='/' className='header-tab'>Venplore - Venture, and Explore!</NavLink>
+      <NavLink activeClassName="selected" exact to='/' className='header-tab'>Home</NavLink>
+      <NavLink activeClassName="selected" exact to='/create' className='header-tab'>Create an activity </NavLink>
+      <NavLink activeClassName="selected" exact to='/about' className='header-tab'>About</NavLink> 
     </div>
   );
 }

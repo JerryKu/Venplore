deaa7504750d753a7b12b14a44ad42ca4ef4478c
Andrew Chen <andrewchen.c44@gmail.com>
2018-04-24 00:10:47 -0700

    Add additional styling

--- client/src/components/describing/About.jsx
+++ client/src/components/describing/About.jsx
@@ -19,34 +19,29 @@ class About extends React.Component {
               lives and am willing to do whatever it takes to get the job
               done. Enjoy your look around!
             </div>
+            <a href='https://github.com/andrewchen44/Venplore' target='_blank'> Link to Code </a>
           </div>
         </div>
         <div className="projects-section">
-          <h2>Past Projects</h2>
+          <h2>What else I have done so far</h2>
           <div className="project-items">
             <div>
-              <h3>Venplore</h3>
-              <img src="https://static1.fitbit.com/simple.b-cssdisabled-jpg.hd7d4926725cd53dd676d7fbbe4f27a52.pack?items=%2Fcontent%2Fassets%2Fadventures%2Fimages%2Fgallery%2Fyosemite8.jpg" />
-              <div>Venplore description go here</div>
-              <a href='https://github.com/andrewchen44/Venplore' target='_blank'> github </a>
-            </div>
-            <div>
+              <img className='project-image'src='https://www.youtube.com/yts/img/yt_1200-vfl4C3T0K.png'/>
               <h3>Youtube emulator</h3>
-              <img src='https://www.youtube.com/yts/img/yt_1200-vfl4C3T0K.png'/>
               <div>youtube description go here</div>
-              <a href='https://github.com/andrewchen44/Client-Service' target='_blank'> github </a>
+              <a href='https://github.com/andrewchen44/Client-Service' target='_blank'> Link to Code </a>
             </div>
             <div>
+              <img className='project-image' src='https://cdn.searchenginejournal.com/wp-content/uploads/2014/09/shutterstock_164716904-760x400.jpg'/>
               <h3>Happenings</h3>
-              <img src='https://cdn.searchenginejournal.com/wp-content/uploads/2014/09/shutterstock_164716904-760x400.jpg'/>
               <div>happenings description goes here</div>
-              <a href='https://github.com/HappeningsHRSF83/Happenings' target='_blank'> github </a>
+              <a href='https://github.com/HappeningsHRSF83/Happenings' target='_blank'> Link to Code </a>
             </div>
             <div>
+              <img className='project-image' src='https://brandcdn.exacttarget.com/blog/uploads/2017/01/Screen-Shot-2017-01-23-at-11.37.38-AM.png'/>
               <h3>Siren</h3>
-              <img src='https://brandcdn.exacttarget.com/blog/uploads/2017/01/Screen-Shot-2017-01-23-at-11.37.38-AM.png'/>
               <div>siren description go here</div>
-              <a href='https://github.com/HappeningsHRSF83/gitPullRebase/tree/dev' target='_blank'> github </a>
+              <a href='https://github.com/HappeningsHRSF83/gitPullRebase/tree/dev' target='_blank'> Link to Code </a>
 
             </div>
           </div>

0583791d1d4d999f29d561cdb00c9afc01dd683a
Andrew Chen <andrewchen.c44@gmail.com>
2018-04-24 14:38:03 -0700

    change paragraph wrapping tags and header font weights

--- client/src/components/describing/About.jsx
+++ client/src/components/describing/About.jsx
@@ -10,7 +10,7 @@ class About extends React.Component {
           <div>
             <h2>Andrew Chen</h2>
             <img src="https://i.imgur.com/iflBcBK.png" />
-            <div>
+            <p>
               Welcome to Venplore! This is the current project that I am
               working to solve the problem of not knowing what to do,
               forever. Venplore is a platform for people to find the perfect
@@ -18,7 +18,7 @@ class About extends React.Component {
               passion for creating things that make an impact on people's
               lives and am willing to do whatever it takes to get the job
               done. Enjoy your look around!
-            </div>
+            </p>
             <a href='https://github.com/andrewchen44/Venplore' target='_blank'> Link to Code </a>
           </div>
         </div>
@@ -29,21 +29,24 @@ class About extends React.Component {
             <div className='project'>
               <img className='project-image'src='https://www.youtube.com/yts/img/yt_1200-vfl4C3T0K.png'/>
               <h3>YouTube Emulator</h3>
-              <div>The Front-End microsorvice for a YouTube emulator that received all requests from the client, routed them to the proper microservice, and returned search results.</div>
+              <h4>Back-End Engineer</h4>
+              <p>The Front-End microsorvice for a YouTube emulator that received all requests from the client, routed them to the proper microservice, and returned search results.</p>
               <a className='code-link' href='https://github.com/andrewchen44/Client-Service' target='_blank'> Link to Code </a>
             </div>
 
             <div className='project'> 
               <img className='project-image' src='https://cdn.searchenginejournal.com/wp-content/uploads/2014/09/shutterstock_164716904-760x400.jpg'/>
               <h3>Happenings</h3>
-              <div>An online platform that allowed users to upload and search for events.</div>
+              <h4>Full Stack Engineer</h4>
+              <p>An online platform that allowed users to upload and search for events.</p>
               <a className='code-link' href='https://github.com/HappeningsHRSF83/Happenings' target='_blank'> Link to Code </a>
             </div>
 
             <div className='project'>
               <img className='project-image' src='https://brandcdn.exacttarget.com/blog/uploads/2017/01/Screen-Shot-2017-01-23-at-11.37.38-AM.png'/>
               <h3>Siren</h3>
-              <div>A web application that allowed users to find and book local artists.</div>
+              <h4>Full Stack Engineer</h4>
+              <p>A web application that allowed users to find and book local artists.</p>
               <a className='code-link' href='https://github.com/HappeningsHRSF83/gitPullRebase/tree/dev' target='_blank'> Link to Code </a>
             </div>
 

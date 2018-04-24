b6b558f3f1aa32bae3fe585edbc0b9f62cb758f7
Andrew Chen <andrewchen.c44@gmail.com>
2018-04-24 15:20:19 -0700

    finish styling projects

--- client/src/components/describing/About.jsx
+++ client/src/components/describing/About.jsx
@@ -27,28 +27,30 @@ class About extends React.Component {
           <div className="project-items">
 
             <div className='project'>
-              <img className='project-image'src='https://www.youtube.com/yts/img/yt_1200-vfl4C3T0K.png'/>
+              <img className='project-image'src='https://www.underconsideration.com/brandnew/archives/youtube_2017_logo_old_elements.png'/>
               <h3>YouTube Emulator</h3>
               <h4>Back-End Engineer</h4>
-              <p>The Front-End microsorvice for a YouTube emulator that received all requests from the client, routed them to the proper microservice, and returned search results.</p>
+              <p>The Front-End microsorvice for a YouTube emulator that received all requests from the client, routes them to the proper microservice, and returns search results.</p>
               <a className='code-link' href='https://github.com/andrewchen44/Client-Service' target='_blank'> Link to Code </a>
-            </div>
-
-            <div className='project'> 
-              <img className='project-image' src='https://cdn.searchenginejournal.com/wp-content/uploads/2014/09/shutterstock_164716904-760x400.jpg'/>
-              <h3>Happenings</h3>
-              <h4>Full Stack Engineer</h4>
-              <p>An online platform that allowed users to upload and search for events.</p>
-              <a className='code-link' href='https://github.com/HappeningsHRSF83/Happenings' target='_blank'> Link to Code </a>
             </div>
 
             <div className='project'>
               <img className='project-image' src='https://brandcdn.exacttarget.com/blog/uploads/2017/01/Screen-Shot-2017-01-23-at-11.37.38-AM.png'/>
               <h3>Siren</h3>
-              <h4>Full Stack Engineer</h4>
+              <h4>Full-Stack Engineer</h4>
               <p>A web application that allowed users to find and book local artists.</p>
               <a className='code-link' href='https://github.com/HappeningsHRSF83/gitPullRebase/tree/dev' target='_blank'> Link to Code </a>
             </div>
+
+            <div className='project'> 
+              <img className='project-image' src='https://cdn.searchenginejournal.com/wp-content/uploads/2014/09/shutterstock_164716904-760x400.jpg'/>
+              <h3>Happenings</h3>
+              <h4>Full-Stack Engineer</h4>
+              <p>An online platform that allowed users to upload and search for events.</p>
+              <a className='code-link' href='https://github.com/HappeningsHRSF83/Happenings' target='_blank'> Link to Code </a>
+            </div>
+
+
 
           </div>
         </div>

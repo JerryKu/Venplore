c52dc519ae153c11e199cda20c562c1a34b0e789
Andrew Chen <andrewchen.c44@gmail.com>
2018-04-25 12:12:33 -0700

    Set navbar active to underline instead of highlight

--- client/src/components/navigating/NavBar.jsx
+++ client/src/components/navigating/NavBar.jsx
@@ -5,9 +5,9 @@ const HeaderBar = (props) => {
   return (
     <div className='header-bar'>
       <NavLink to='/' className='header-tab'>Venplore - Venture, and Explore!</NavLink>
-      <NavLink activeClassName="selected" exact to='/' className='header-tab'>Home</NavLink>
-      <NavLink activeClassName="selected" exact to='/create' className='header-tab'>Create an activity </NavLink>
-      <NavLink activeClassName="selected" exact to='/about' className='header-tab'>About</NavLink> 
+      <NavLink activeClassName="selected" exact to='/' className='header-tab'> <span> Home</span></NavLink>
+      <NavLink activeClassName="selected" exact to='/create' className='header-tab'> <span>Create an activity</span> </NavLink>
+      <NavLink activeClassName="selected" exact to='/about' className='header-tab'> <span>About</span></NavLink> 
     </div>
   );
 }

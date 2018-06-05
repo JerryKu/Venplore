c8f59a23da651a5606ac5b87aa3993f12e790528
Andrew Chen <andrewchen.c44@gmail.com>
2018-06-05 12:30:41 -0700

    Create very basic login and sign up components

---
+++ client/src/components/authenticating/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = (props) => {
+  return (
+    <div>
+      <form>
+        Email: <input type='text' />
+        Password: <input type='password' />
+      </form>
+    </div>
+  )
+}
+
+export default Login;

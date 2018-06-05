c4628259d0cf426e58ad77ba31ab6bc2f5c27407
Andrew Chen <andrewchen.c44@gmail.com>
2018-06-05 16:03:21 -0700

    Add facebook authentication scripts and buttons

--- client/src/components/authenticating/Login.jsx
+++ client/src/components/authenticating/Login.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 const Login = (props) => {
-  return (
-    <div>
+  return <div>
       <form>
-        Email: <input type='text' />
-        Password: <input type='password' />
+        Email: <input type="text" />
+        Password: <input type="password" />
       </form>
-    </div>
-  )
+      <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false" />
+    </div>;
 }
 
 export default Login;

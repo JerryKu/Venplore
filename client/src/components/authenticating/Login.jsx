059ee19a3db54413a1b2e3d83c8c8c7032bc173d
Andrew Chen <andrewchen.c44@gmail.com>
2018-06-05 23:56:53 -0700

    Fix facebook login button

--- client/src/components/authenticating/Login.jsx
+++ client/src/components/authenticating/Login.jsx
@@ -6,7 +6,7 @@ const Login = (props) => {
         Email: <input type="text" />
         Password: <input type="password" />
       </form>
-      <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false" />
+      <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false" />
     </div>;
 }
 

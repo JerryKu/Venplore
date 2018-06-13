f7ef76516487397ab7b37a88dca256c778efd5a4
Andrew Chen <andrewchen.c44@gmail.com>
2018-06-13 13:48:51 -0700

    Implement signup authentication with passport

--- server/routes.js
+++ server/routes.js
@@ -4,10 +4,23 @@ module.exports = function(app, passport) {
   // app.post('/login', do all our passport stuff here);
 
   // process the signup form
-  app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect : '/finding', 
-    failureRedirect : '/signup', 
-  }));
+  app.post('/signup', 
+  (req, res, next) => {
+    passport.authenticate('local-signup', (err, user, responseMessage) => {
+      if(err) { return res.json({ message: err }) }
+      if(!user) { 
+        return res.status(200).json({ 
+        responseMessage,
+        redirectUrl: '/signup'
+       })
+      }
+      return res.status(200).json({
+        responseMessage,
+        redirectUrl: '/login'
+      })
+    })(req, res, next);
+  }
+);
 
 
   // =====================================

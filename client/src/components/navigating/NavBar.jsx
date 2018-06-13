d0efdf4f5695ea256ace34717c8a413f84d4ddef
Andrew Chen <andrewchen.c44@gmail.com>
2018-06-12 20:06:20 -0700

    Reactivate sign up and login tabs

--- client/src/components/navigating/NavBar.jsx
+++ client/src/components/navigating/NavBar.jsx
@@ -12,16 +12,9 @@ const HeaderBar = ({ dispatch} ) => {
       <NavLink activeClassName="selected" exact to='/' className='header-tab'> <span>Home</span></NavLink>
       <NavLink activeClassName="selected" exact to='/finding' className='header-tab'> <span>Find An Activity</span></NavLink>
       <div onClick={createActivity} className='header-tab'> <span>Create an activity</span> </div>
-<<<<<<< HEAD
-      {/* <NavLink activeClassName="selected" exact to='/login' className='header-tab'> <span>Login</span></NavLink>
-      <NavLink activeClassName="selected" exact to='/signup' className='header-tab'> <span>Sign Up</span></NavLink>
-      */}
-=======
-      <NavLink activeClassName="selected" exact to='/about' className='header-tab'> <span>About</span></NavLink>
       <NavLink activeClassName="selected" exact to='/login' className='header-tab'> <span>Login</span></NavLink>
       <NavLink activeClassName="selected" exact to='/signup' className='header-tab'> <span>Sign Up</span></NavLink>
-      <NavLink activeClassName="selected" exact to='/home' className='header-tab'> <span>Home</span></NavLink>
->>>>>>> started landing page, added landing page css, added url-loader for images
+     
     </div>
   );
 }

04e8a592274f7fcbc3463a29198fbd561ccbe836
Jerry Ku <jerryku1220@gmail.com>
2018-06-21 12:54:49 -0700

    fixed some grid issues at smaller BP, changed JS styling to CSS, headerbar is sticky again

--- client/src/components/landingPage/featuredActivities.jsx
+++ client/src/components/landingPage/featuredActivities.jsx
@@ -11,15 +11,11 @@ class FeaturedActivities extends Component {
       this.props.history.push('/finding');
       window.scrollTo(0, 0);
     }
-    componentDidUpdate(){
-      if(document.body.clientWidth >= 1024){
-          document.getElementsByClassName('activity-card-container')[0].style.gridColumn = "span 2";
-      }
-    }
     render(){
       return (
         <div className="featuredActivities grid-full">
-          <div className="featured-header"><h3>things to do in</h3> <h1>San Jose</h1></div><div className="relative featured-search-bar"><SearchBar {...this.props}/></div>
+          <div className="featured-header"><h3>things to do in</h3> <h1>San Jose</h1></div>
+          <div className="relative featured-search-bar"><SearchBar {...this.props}/></div>
           {this.props.allActivities.map((activity, index)=>(
             <div
               className="activity-card-container"
--- client/src/components/navigating/NavBar.jsx
+++ client/src/components/navigating/NavBar.jsx
@@ -8,26 +8,18 @@ const HeaderBar = ({ dispatch} ) => {
     dispatch(setCreationState('creating'));
   }
   return (
-    <div className="grid-full">
-      {document.body.clientWidth >= 1024 ?
-        <div className="header-bar">
-          <div className='header-links'>
-            <div className="header-tab header-city">My City: San Jose</div>
-            <NavLink exact to='/' className='vp-logo'><img src={VenploreLogo}/></NavLink>
-            <NavLink activeClassName="selected" exact to='/' className='header-tab nav-links'> <span>Home</span></NavLink>
-            <NavLink activeClassName="selected" exact to='/finding' className='header-tab nav-links'> <span>Explore</span></NavLink>
-            <div onClick={createActivity} className='header-tab nav-links'> <span>Create</span> </div>
-            {/* <NavLink activeClassName="selected" exact to='/login' className='header-tab'> <span>Login</span></NavLink>
-            <NavLink activeClassName="selected" exact to='/signup' className='header-tab'> <span>Sign Up</span></NavLink>
-            */}
-          </div>
+      <div className="header-bar grid-full">
+        <div className='header-links'>
+          <div className="header-tab header-city">My City: San Jose</div>
+          <NavLink exact to='/' className='vp-logo'><img src={VenploreLogo}/></NavLink>
+          <NavLink activeClassName="selected" exact to='/' className='header-tab nav-links'> <span>Home</span></NavLink>
+          <NavLink activeClassName="selected" exact to='/finding' className='header-tab nav-links'> <span>Explore</span></NavLink>
+          <div onClick={createActivity} className='header-tab nav-links'> <span>Create</span> </div>
+          {/* <NavLink activeClassName="selected" exact to='/login' className='header-tab'> <span>Login</span></NavLink>
+          <NavLink activeClassName="selected" exact to='/signup' className='header-tab'> <span>Sign Up</span></NavLink>
+          */}
         </div>
-        :
-        <div className="mobile-header-bar">
-          <img className="absolute" src={VenploreLogo}/>
-        </div>
-      }
-    </div>
+      </div>
   );
 }
 

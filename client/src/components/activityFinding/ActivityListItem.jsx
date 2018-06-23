9c9ae54f8845f61277b54b7774933d3bdf337f08
Andrew Chen <andrewchen.c44@gmail.com>
2018-06-22 19:32:09 -0700

    shorted text that was too long for activity list item

--- client/src/components/activityFinding/ActivityListItem.jsx
+++ client/src/components/activityFinding/ActivityListItem.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const ActivityListItem = ({image, name, score, description}) => {
-  return (
-    <div className='activity-list-content'>
-      <img className='activity-list-image' src={image} />
-      <div className='content-container'>
-        <div className="large-title text-orange">{name}</div>
-        <div>Adventure Score: {score}</div>
-        <div>{description}</div>
+  const shortenedDescription = description.length > 380 ? description.substring(0, 377) + '...': description; 
+  return <div className="activity-list-content">
+      <img className="activity-list-image" src={image} />
+      <div className="content-container">
+        <div className="large-title text-orange">
+          {name}: {score}
+        </div>
+        <div>{shortenedDescription}</div>
       </div>
-
-    </div>)
+    </div>;
 }
 export default ActivityListItem;

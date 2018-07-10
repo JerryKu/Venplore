598d645964b8ee8b1de6e3770ccac57d945a52d8
Jerry Ku <jerryku1220@gmail.com>
2018-07-10 14:39:36 -0700

    minor UI changes

--- client/src/components/landingPage/featuredActivityCard.jsx
+++ client/src/components/landingPage/featuredActivityCard.jsx
@@ -7,13 +7,18 @@ class FeaturedActivityCard extends Component {
   }
   render() {
     return (
+<<<<<<< HEAD
       <div className="activity-card absolute">
         <img className="activity-card-img" src={this.props.image !== '' ? this.props.image : 'http://www.independentmediators.co.uk/wp-content/uploads/2016/02/placeholder-image-768x576.jpg'}/>
+=======
+      <div className="activity-card relative">
+        <img className="activity-card-img" src={this.props.image}/>
+>>>>>>> minor UI changes
         <div className="activity-card-hover relative">
-          <div className="activity-card-description">
+          {/*<div className="activity-card-description">
             <p className="activity-card-text">{
               this.props.description.length > 150 ? this.props.description.substring(0, 150) + "..." : this.props.description.substring(0, 150)}</p>
-          </div>
+          </div> */}
           <div className="activity-card-title">
             <h2>{this.props.name}</h2>
           </div>

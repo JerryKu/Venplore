b4b98b00dd22aa47612b769d024be10d49dd1d4c
Andrew Chen <andrewchen.c44@gmail.com>
2018-07-12 17:59:10 -0700

    Add additional styling

--- client/src/components/eventCreating/RadioButtons.jsx
+++ client/src/components/eventCreating/RadioButtons.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 const RadioButtons = ({options, title, filter, handleChange}) => {
   return (
-    <div className='radio-buttons'>
-      {title}
-      {options.map((option) => {
-        return (
-          <label key={option} className='radio-button'>
-            {option} <input type='radio' value={option} name={filter} onChange={handleChange.bind(this, filter)}/>
-          </label>
-        )
-      })}
+    <div className='radio-input-row'>
+      <div className='radio-input-title'> {title} </div>
+      <div className='radio-buttons'>
+        {options.map((option) => {
+          return (
+            <label key={option} className='radio-button'>
+              {option} <input type='radio' value={option} name={filter} onChange={handleChange.bind(this, filter)} />
+            </label>
+          )
+        })}
+      </div>
+
     </div>
   )
 }
